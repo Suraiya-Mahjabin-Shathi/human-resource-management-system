ad0f719402d052e8c3ad6cc6dd31851d7bf4128f
Suraiya-Mahjabin-Shathi <suraiya.shathi.1234@gmail.com>
2023-01-26 09:54:57 +0600

    templating

---
+++ resources/views/backend/master.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+@include('backend.fixed.sidebar')
+
+
+
+<body>
+    <div class="container-fluid position-relative bg-white d-flex p-0">
+
+
+     <!-- Navbar Start -->
+           
+    
+     <!-- Navbar End -->
+
+
+
+        <!-- Content Start -->
+        <div class="content">
+
+
+            @include('backend.fixed.header')
+
+            
+            <!-- Sale & Revenue Start -->
+            @yield('content')
+            <!-- Sale & Revenue End -->
+            
+        </div>
+
+        <!-- Content End -->
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/chart/chart.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/tempusdominus/js/moment.min.js"></script>
+    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrganizationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [HomeController::class,'dashboard'])->name('dashboard');
+Route::get('/organization', [OrganizationController::class,'list'])->name('organization');
